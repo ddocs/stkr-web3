966cc44e303cc29d9d3314bf118da8baae47a2e4
Süleyman Kardaş <skardas@gmail.com>
2020-09-25 01:56:59 +0300

    Provider pending list removed from governance

--- src/views/pages/Governance.vue
+++ src/views/pages/Governance.vue
@@ -35,52 +35,7 @@
         </template>
       </vs-table>
     </vs-card>
-    <vs-card>
-      <vs-navbar-title>Nodes to approve</vs-navbar-title>
-      <vs-table class="col-lg-6" :data="nodes" search>
 
-        <template slot="thead">
-          <vs-th sort-key="name">Provider</vs-th>
-          <vs-th sort-key="provider">Validator Count</vs-th>
-        </template>
-
-        <template slot-scope="{data}">
-          <vs-tr v-for="(tr, indextr) in nodes" :key="indextr">
-
-            <!--          <vs-td :data="data[indextr].name">-->
-            <!--            {{ data[indextr].name }}-->
-            <!--          </vs-td>-->
-
-            <!--          <vs-td :data="data[indextr].provider">-->
-            <!--            {{ data[indextr].provider }}-->
-            <!--          </vs-td>-->
-
-            <!--          <vs-td :data="data[indextr].status">-->
-            <!--            {{ data[indextr].status }}-->
-            <!--          </vs-td>-->
-
-            <!--          <vs-td :data="data[indextr].totalStake">-->
-            <!--            {{ data[indextr].totalStake }} ETH-->
-            <!--          </vs-td>-->
-
-            <!--          <vs-td :data="data[indextr].fee">-->
-            <!--            {{ data[indextr].fee }} USDT-->
-            <!--          </vs-td>-->
-
-            <!--          <vs-td :data="data[indextr].reward">-->
-            <!--            {{ data[indextr].reward || 0 }} ETH-->
-            <!--          </vs-td>-->
-
-            <vs-td>
-              <vs-button v-show="data[indextr].status === 'Pending'" @click="poolModal = true; modalData = data[indextr]">
-                Join
-              </vs-button>
-            </vs-td>
-
-          </vs-tr>
-        </template>
-      </vs-table>
-    </vs-card>
   </vs-row>
 </template>
 
