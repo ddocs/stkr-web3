daa8c9af3959243a9aacb8b067781b417c2737c0
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-10-11 18:57:24 +0300

    [FD-2895] Reformatting

--- next/src/components/TableComponents/TableBody/TableBodyStyles.tsx
+++ next/src/components/TableComponents/TableBody/TableBodyStyles.tsx
@@ -13,39 +13,31 @@ export const useTableBodyStyles = makeStyles<
 >(theme => ({
   bodyWrapper: {
     position: 'relative',
-
     width: '100%',
     height: props =>
       props.defense
         ? `calc(100% - ${DEFENSE_HEADER_HEIGHT}px)`
         : `calc(100% - ${HEADER_HEIGHT}px)`,
     marginTop: 2,
-
     paddingLeft: props => (props.paddingCollapse ? theme.spacing(4) : 0),
     paddingRight: props => (props.paddingCollapse ? theme.spacing(4) : 0),
-
     boxSizing: 'border-box',
-
     overflow: 'hidden',
-
     [theme.breakpoints.down('md')]: {
       paddingLeft: props => (props.paddingCollapse ? theme.spacing(3) : 0),
       paddingRight: props => (props.paddingCollapse ? theme.spacing(3) : 0),
     },
-
     [theme.breakpoints.down('xs')]: {
       paddingLeft: props => (props.paddingCollapse ? theme.spacing(2) : 0),
       paddingRight: props => (props.paddingCollapse ? theme.spacing(2) : 0),
     },
   },
-
   body: {
     display: 'grid',
     gridTemplateColumns: props =>
       props.customCell ? props.customCell : `repeat(${props.count}, 1fr)`,
     gridRowGap: 2,
     alignItems: 'stretch',
-
     width: '100%',
   },
 }));
