6a87666bf4ca8695428c7b9158abb9dd0a327f1b
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-10-15 00:40:46 +0300

    [FD-2895] Refactor styles

--- next/src/UiKit/Curtains/Curtains.tsx
+++ next/src/UiKit/Curtains/Curtains.tsx
@@ -4,8 +4,8 @@ import { Container, ContainerProps } from '@material-ui/core';
 export const Curtains = React.forwardRef<
   HTMLElement,
   ContainerProps & { component?: React.ElementType; children: React.ReactNode }
->(({ children, component = 'div', ...props }, ref) => (
-  <Container component={component} {...props} ref={ref}>
+>(({ children, className, component = 'div', ...props }, ref) => (
+  <Container className={className} component={component} {...props} ref={ref}>
     {children}
   </Container>
 ));
