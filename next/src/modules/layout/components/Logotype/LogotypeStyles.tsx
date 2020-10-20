449b87a56103e9f411b8fe6df36ce8ee1831c182
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-10-20 19:48:03 +0300

    [FD-2895] Repair prod styles

--- next/src/modules/layout/components/Logotype/LogotypeStyles.tsx
+++ next/src/modules/layout/components/Logotype/LogotypeStyles.tsx
@@ -1,50 +1,41 @@
 import { fade, makeStyles } from '@material-ui/core/styles';
-import { Theme } from '@material-ui/core';
 
-export const useLogotypeStyles = makeStyles<Theme>(theme => ({
+export const useLogotypeStyles = makeStyles(theme => ({
   component: {
-    display: 'grid',
+    '&&': {
+      display: 'grid',
+      margin: 0,
+      marginRight: 'auto',
+    },
     gridTemplateColumns: 'auto auto',
     gridTemplateAreas: '"link company"',
     gridColumnGap: theme.spacing(2.5),
     alignItems: 'center',
-
-    margin: 0,
-    marginRight: 'auto',
-
     '& > span': {
       whiteSpace: 'nowrap',
     },
   },
-
   withDivider: {
     gridTemplateColumns: 'auto auto auto',
     gridTemplateAreas: '"link divider company"',
-
     '&::before': {
       position: 'relative',
       content: '""',
-
       gridArea: 'divider',
-
       display: 'block',
       width: 1,
       height: theme.spacing(4.5),
-
       backgroundColor: fade(theme.palette.text.primary, 0.1),
     },
   },
-
   link: {
     gridArea: 'link',
     width: 103,
-
     '& svg': {
       width: '100%',
       height: 'auto',
     },
   },
-
   active: {
     pointerEvents: 'none',
   },
