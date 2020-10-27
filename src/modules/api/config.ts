49efb9d0d302ad273533a3502180c76dce8e6d7d
Dmitry Savonin <dmitry@anrk.com>
2020-10-27 22:48:36 +0300

    updated smart contracts

--- src/modules/api/config.ts
+++ src/modules/api/config.ts
@@ -35,10 +35,10 @@ export const DEVELOP_CONFIG: StkrConfig = {
     chainId: '5',
   },
   contractConfig: {
-    microPoolContract: '0x206B7200e6E497E96c592c760a50f030fe146c28',
-    ankrContract: '0x8Ca834Db937a221B8858C58ede272C6d5A6d921d',
-    stakingContract: '0x51b0b6b72684C68c8B8091d55ba155C503D44d78',
-    systemContract: '0x40Eec3627A95c8fb969Bbf6769817cA3E021C289',
+    microPoolContract: '0x605B652E2E824D40eFc556073a940E9E0a635Ccc',
+    ankrContract: '0xb8Ca76ad70BA333a6E748CB2e11529FB681607Bc',
+    stakingContract: '0x496a9BABf517DD43787888a6a036cAaa31dAe3Ef',
+    systemContract: '0x971fd574D320020B8715fFc260268048321dD29B',
   },
   gatewayConfig: {
     baseUrl: 'https://api.stkr-dev.ankr.com/',
@@ -51,10 +51,10 @@ export const GOERLI_CONFIG: StkrConfig = {
     chainId: '5',
   },
   contractConfig: {
-    microPoolContract: '0x206B7200e6E497E96c592c760a50f030fe146c28',
-    ankrContract: '0x8Ca834Db937a221B8858C58ede272C6d5A6d921d',
-    stakingContract: '0x51b0b6b72684C68c8B8091d55ba155C503D44d78',
-    systemContract: '0x40Eec3627A95c8fb969Bbf6769817cA3E021C289',
+    microPoolContract: '0xb89501803ECA443395e812a2809C1E983571f2Bb',
+    ankrContract: '0xFbA94aC31054a00000a9a87A10E45C7e980FFC62',
+    stakingContract: '0x4a62eB9556cE8f5aa5391C701Dee45506ac5E18b',
+    systemContract: '0x45D3fAb725aa73308D52990BcB66106542004aEe',
   },
   gatewayConfig: {
     baseUrl: 'https://api.stkr-goerli.ankr.com/',
