356561af04bc2dbeb4c9178d16e2117467d0e9c6
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-10-30 16:00:05 +0300

    [FD-2968] Move platforms icons to files

--- src/modules/provider/components/NodeList/NodeListStyles.ts
+++ src/modules/provider/components/NodeList/NodeListStyles.ts
@@ -1,9 +1,16 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import { Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-export const useNodeListStyles = makeStyles<Theme>(() => ({
+export const useNodeListStyles = makeStyles<Theme>(theme => ({
   component: {},
   table: {
     width: '100%',
+  },
+  icon: {
+    padding: 0,
+    marginRight: theme.spacing(1.5),
+    '&.Mui-disabled': {
+      opacity: 0.5,
+    },
   },
 }));
