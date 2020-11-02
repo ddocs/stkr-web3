395c2529c22bfa6d4e3df5b3f6ebbbbe02ea4621
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-11-02 17:26:39 +0300

    [FD-2968] Reset error on show notification

--- src/components/MutationErrorHandler/MutationErrorHandler.tsx
+++ src/components/MutationErrorHandler/MutationErrorHandler.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { NotificationActions } from '../../store/actions/NotificationActions';
 import { getErrorMessage } from '../QueryError/QueryError';
 import { useMutation } from '@redux-requests/react';
+import { resetRequests } from '@redux-requests/core';
 
 interface IMutationErrorHandlerProps {
   type: string;
@@ -12,6 +13,15 @@ interface IMutationErrorHandlerProps {
 export const MutationErrorHandler = ({ type }: IMutationErrorHandlerProps) => {
   const { error } = useMutation({ type });
   const dispatch = useDispatch();
+
+  useEffect(
+    () => () => {
+      if (error) {
+        dispatch(resetRequests([type]));
+      }
+    },
+    [dispatch, error, type],
+  );
 
   useEffect(() => {
     if (error) {
