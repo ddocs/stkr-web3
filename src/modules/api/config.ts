b9f43072494904c6cf82cc30ea841a38a98dc061
Dmitry Savonin <dmitry@anrk.com>
2020-11-09 22:19:39 +0300

    added config for mainnet

--- src/modules/api/config.ts
+++ src/modules/api/config.ts
@@ -37,7 +37,17 @@ export const GOERLI_CONFIG: IStkrConfig = {
 };
 
 export const MAINNET_CONFIG: IStkrConfig = {
-  ...LOCAL_CONFIG,
+  providerConfig: {
+    networkId: '1',
+    chainId: '1',
+  },
+  contractConfig: {
+    aethContract: '0xE95A203B1a91a908F9B9CE46459d101078c2c3cb',
+    microPoolContract: '0x84db6eE82b7Cf3b47E8F19270abdE5718B936670',
+    ankrContract: '0x8290333cef9e6d528dd5618fb97a76f268f3edd4',
+    stakingContract: '0x0000000000000000000000000000000000000000',
+    systemContract: '0x3bFce37B5401BEF13C78830D3A9FB14294d18c4F',
+  },
   baseUrl: 'https://api.stkr.ankr.com/',
   configFile: 'mainnet.json',
 };
