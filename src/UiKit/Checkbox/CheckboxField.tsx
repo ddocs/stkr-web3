141f900ff5d02628ef75ec4a3e6a94bd9c0e71d3
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-11-10 17:31:06 +0300

    [STKR-73] Checkbox makeup

--- src/UiKit/Checkbox/CheckboxField.tsx
+++ src/UiKit/Checkbox/CheckboxField.tsx
@@ -1,53 +1,37 @@
 import React, { ReactNode } from 'react';
 import { FieldRenderProps } from 'react-final-form';
-import {
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  Typography,
-} from '@material-ui/core';
+import { Checkbox, FormControl, FormControlLabel } from '@material-ui/core';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import { getErrorText, hasError } from '../../common/utils/form';
 import { useCheckboxStyles } from './CheckboxFieldStyles';
-import { omit } from '../../common/utils/typeUtils';
+import { Body2 } from '../Typography';
 
 interface ISwitchFieldProps extends FieldRenderProps<HTMLElement> {
   label: string;
-  labelPlacement: string;
   children?: ReactNode;
-  showErrorText?: boolean;
 }
 
 export const CheckboxField = ({
   label,
-  labelPlacement,
   input,
   meta,
   children,
-  showErrorText = true,
-  ...rest
 }: ISwitchFieldProps & any) => {
   const classes = useCheckboxStyles();
 
   return (
-    <div className={classes.root}>
+    <div>
       <FormControlLabel
-        classes={{
-          root: classes.formControlLabel,
-        }}
-        label={
-          children || <Typography className={classes.label}>{label}</Typography>
-        }
+        label={children || <Body2>{label}</Body2>}
+        classes={{ root: classes.root }}
         control={
           <FormControl error={!!meta.error} className={classes.checkbox}>
-            <Checkbox color="secondary" {...rest} {...omit(input, 'value')} />
+            <Checkbox color="primary" {...input} />
           </FormControl>
         }
       />
-      {showErrorText && hasError(meta) && (
-        <FormHelperText error={true} classes={{ root: classes.error }}>
-          {getErrorText(meta)}
-        </FormHelperText>
+      {hasError(meta) && (
+        <FormHelperText error={true}>{getErrorText(meta)}</FormHelperText>
       )}
     </div>
   );
--- src/UiKit/Checkbox/CheckboxFieldStyles.tsx
+++ src/UiKit/Checkbox/CheckboxFieldStyles.tsx
@@ -1,21 +1,10 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 export const useCheckboxStyles = makeStyles<Theme>(theme => ({
-  formControlLabel: {
+  root: {
     alignItems: 'flex-start',
-    marginLeft: -theme.spacing(0.5),
-    marginRight: theme.spacing(1),
   },
   checkbox: {
-    marginTop: -9,
-  },
-  error: {
-    marginTop: -5,
-    marginBottom: 16,
-  },
-  checked: {},
-  label: {
-    color: theme.palette.primary.main,
-    marginLeft: 4,
+    marginTop: theme.spacing(-1),
   },
 }));
