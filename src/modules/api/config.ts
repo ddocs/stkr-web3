f0f7843a9589015ddef3a4eee4b4e9ab405e21b5
Dmitry Savonin <dmitry@anrk.com>
2020-11-28 00:41:57 +0300

    updated goerli pool contract

--- src/modules/api/config.ts
+++ src/modules/api/config.ts
@@ -33,12 +33,12 @@ export const DEVELOP_CONFIG: IStkrConfig = {
 export const GOERLI_CONFIG: IStkrConfig = {
   ...LOCAL_CONFIG,
   contractConfig: {
-    aethContract: '0x35F50849eEe33632cD948A165C75523Fd8E6B16B',
-    microPoolContract: '0xDDEC45249efb72CbFe78f1Ad6DBf596DB6782CcD',
+    aethContract: '0x63dC5749fa134fF3B752813388a7215460a8aB01',
+    microPoolContract: '0x5ea4C3a6CA22B38a1D6776329bb8b4073C157B27',
     microPoolBlock: '3777622',
     ankrContract: '0x23f438234655Bf317af71088e135104A5532F937', // old
     stakingContract: '0xe48cDc6146722C045566c6B614263e22D9bF1b50', // old
-    systemContract: '0x19Ec552FB068D48de1A494C6655fc32bB00B2632',
+    systemContract: '0x19F75408fc6F08f22156a26c279Db7F033779d94',
   },
   baseUrl: 'https://api.goerli.stkr.io/',
 };
