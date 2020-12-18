e557c7cc09d24a7d484bc60fd6e47a319f803c87
Aleksandr Ishchenko <aleksandr@ankr.com>
2020-12-18 17:35:32 +0300

    [FD-3115] Preview

--- src/modules/lobby/components/VideoTutorial/VideoTutorial.tsx
+++ src/modules/lobby/components/VideoTutorial/VideoTutorial.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useVideoTutorial } from './VideoTutorialStyles';
 import { Curtains } from '../../../../UiKit/Curtains';
+import ReactPlayer from 'react-player';
+import { ReactComponent as PlayIcon } from './assets/play.svg';
+
+const URL = 'https://cdn.stkr.io/assets/stkr_presentation.mp4';
 
 export const VideoTutorial = () => {
   const classes = useVideoTutorial();
   console.log('classes', classes);
 
+  const handlePlay = useCallback(() => {
+    console.log('handlePlay');
+  }, []);
+
   return (
     <section>
-      <Curtains>VideoTutorial</Curtains>
+      <Curtains className={classes.root}>
+        <div onClick={handlePlay}>
+          <ReactPlayer
+            url={URL}
+            controls={false}
+            playing={true}
+            muted={true}
+            volume={0.7}
+            loop={true}
+            width="100%"
+            height={740}
+            config={{
+              youtube: {
+                playerVars: {
+                  controls: 0,
+                  iv_load_policy: 3,
+                  modestbranding: 1,
+                  disablekb: 1,
+                },
+              },
+            }}
+            onPlay={handlePlay}
+          />
+          <PlayIcon className={classes.play} />
+        </div>
+      </Curtains>
     </section>
   );
 };
--- src/modules/lobby/components/VideoTutorial/VideoTutorialStyles.ts
+++ src/modules/lobby/components/VideoTutorial/VideoTutorialStyles.ts
@@ -1,3 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useVideoTutorial = makeStyles(() => ({}));
+export const useVideoTutorial = makeStyles(() => ({
+  root: {
+    position: 'relative',
+  },
+  play: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    margin: 'auto',
+  },
+}));
