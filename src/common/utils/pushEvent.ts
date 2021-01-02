14f11625302f3d6726b2a7ef37295fe6ece21249
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-01-02 16:49:01 +0300

    [FD-3150] Remove debugger

--- src/common/utils/pushEvent.ts
+++ src/common/utils/pushEvent.ts
@@ -1,7 +1,6 @@
 import { isProd } from './isProd';
 
 export function pushEvent(event: string, values: any) {
-  debugger;
   if (!window.dataLayer) {
     if (isProd()) {
       console.warn("Data layer hasn't been initialized");
