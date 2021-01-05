46aea6b20e80b906a8464d13ef88a98467ab56ae
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-01-05 16:26:08 +0300

    [FD-3150] Fix PrivateRoute

--- src/UiKit/PrivateRoute.tsx
+++ src/UiKit/PrivateRoute.tsx
@@ -3,20 +3,20 @@ import { Redirect, RouteProps } from 'react-router-dom';
 import { INDEX_PATH } from '../common/const';
 import { useStore } from 'react-redux';
 import { IStoreState } from '../store/reducers';
+import { Route } from 'react-router-dom';
 
 interface IPrivateRouteProps extends RouteProps {
   component: any;
 }
 
 export const PrivateRoute = (props: IPrivateRouteProps) => {
-  const { component: CurrentComponent, ...rest } = props;
   const store = useStore<IStoreState>();
   const {
     user: { isConnected },
   } = store.getState();
 
   if (isConnected) {
-    return <CurrentComponent {...rest} />;
+    return <Route {...props} />;
   }
 
   return <Redirect to={INDEX_PATH} />;
