ba1af88a21885259bdf42c4958c0bfd06f8fcf38
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-01-21 14:26:07 +0300

    [FD-3217] Update chart data

--- src/modules/provider/const.ts
+++ src/modules/provider/const.ts
@@ -5,4 +5,4 @@ const ANKR_STKR_NODE_VERSION =
 const ANKR_STKR_NODE_APP_VERSION =
   process.env.REACT_APP_ANKR_STKR_NODE_APP_VERSION ?? '1.0.5';
 
-export const ANKR_DEPLOY_PATH = `${ANKR_ROOT}/apps/deploy?name=stkr&repository=stable&type=chart&version=${ANKR_STKR_NODE_VERSION}&appversion=v${ANKR_STKR_NODE_APP_VERSION}`;
+export const ANKR_DEPLOY_PATH = `${ANKR_ROOT}/apps/deploy?name=ankr-eth2.0&repository=stable&type=chart&version=${ANKR_STKR_NODE_VERSION}&appversion=v${ANKR_STKR_NODE_APP_VERSION}`;
