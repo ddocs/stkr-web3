7522a4f0fce7d59c8709eec768500d0b09be1499
giorgi.pro <pangular@giorgi.pro>
2021-01-21 12:16:25 +0100

    [FD-3196] Empty Payment History Placeholder (#59)
    * [FD-3196] - update syles
    
    * FD-3196 - change buttons to links
    
    * FD-3196 - display Metamask error user friendly way
    
    Co-authored-by: giorgi <giorgi@ankr.com>

--- src/modules/provider/components/NotEnoughBalance/NotEnoughBalanceStyles.ts
+++ src/modules/provider/components/NotEnoughBalance/NotEnoughBalanceStyles.ts
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useEmptyNodeListStyles = makeStyles(theme => ({
+  root: {},
   title: {
     marginBottom: theme.spacing(3),
   },
