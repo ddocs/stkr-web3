9a7daea3b02c48593c1fc39af55fb06c5bff7a6a
Aleksandr Ishchenko <aleksandr@ankr.com>
2021-01-30 20:52:44 +0300

    [FD-3219] index: 10

--- src/modules/convert/screens/Convert/ConvertStyles.tsx
+++ src/modules/convert/screens/Convert/ConvertStyles.tsx
@@ -1,14 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useConvertStyles = makeStyles(theme => ({
-  label: {
-    fontSize: 20,
-  },
-  value: {
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  divider: {
-    margin: theme.spacing(0, -11.5, 7, -11.5),
-  },
-}));
+export const useConvertStyles = makeStyles(
+  theme => ({
+    label: {
+      fontSize: 20,
+    },
+    value: {
+      fontSize: 22,
+      fontWeight: 'bold',
+    },
+    divider: {
+      margin: theme.spacing(0, -11.5, 7, -11.5),
+    },
+  }),
+  { index: 10 },
+);
