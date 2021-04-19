2f1773f920c07709b3dca909b3c1be3a973298bf
giorgi <giorgi@ankr.com>
2021-04-19 13:29:43 +0200

    [FD-3355] - align button

--- src/modules/governance/ProjectList.tsx
+++ src/modules/governance/ProjectList.tsx
@@ -135,7 +135,7 @@ export const ProjectList = () => {
               </Button>
             </div>
           </Paper>
-          <Box display="flex" justifyContent="center">
+          <Box display="flex" justifyContent="center" mb={6.5}>
             <Mutation
               type={GovernanceActionTypes.CLAIM_ANKR}
               showLoaderDuringRefetch={true}
