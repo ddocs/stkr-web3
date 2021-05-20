f19bcd6fbfc4c1953e304d940f31a22b0bf61456
giorgi <giorgi@ankr.com>
2021-05-20 16:23:55 +0200

    [FE-3502] - clean up

--- src/modules/lobby/components/Banner/Banner.tsx
+++ src/modules/lobby/components/Banner/Banner.tsx
@@ -1,12 +1,5 @@
-import { ArrowForward } from '@material-ui/icons';
 import classNames from 'classnames';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { ABOUT_AETH_PATH } from '../../../../common/const';
-import { t } from '../../../../common/utils/intl';
-import { InView } from '../../../../components/InView';
-import { Button } from '../../../../UiKit/Button';
-import { Curtains } from '../../../../UiKit/Curtains';
+import React from 'react';
 import { useBannerStyles } from './BannerStyles';
 import { ReactComponent as Panel1 } from './assets/panel_1.svg';
 import { ReactComponent as Panel2 } from './assets/panel_2.svg';
