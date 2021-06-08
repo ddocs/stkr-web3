33a59d88dda78e2f66a19d6978ae28c124996c76
Konstantin Biskub <konstantin@ankr.com>
2021-06-08 16:58:36 +0300

    [FE-3430] Code clearning

--- src/components/ZendeskMounter/useZendeskOrganization.ts
+++ src/components/ZendeskMounter/useZendeskOrganization.ts
@@ -4,11 +4,6 @@ import { IStoreState } from '../../store/reducers';
 export const useZendeskOrganization = (doFetch = true) => {
   const { zendeskOrganization, fetchOrganizationStatus } = useSelector(
     (state: IStoreState) => {
-      // const currentTeamId = state.team.currentTeamId ?? '';
-      // return {
-      //   zendeskOrganization: state.zendesk.organizations?.[currentTeamId],
-      //   fetchOrganizationStatus: state.zendesk.fetchOrganizationStatus,
-      // };
       return {
         zendeskOrganization: 'zendeskOrganization',
         fetchOrganizationStatus: 'fetchOrganizationStatus',
