2f7763744f3d0ba99023e52c50ce5f3115e6c6db
antroll <panov-rabota@mail.ru>
2021-07-27 22:44:46 +0300

    fix: basic timer refactoring
    The previous implementation counted the number of days in an incomplete
    month. If the days were more than a month, the number of months was
    indicated separately and was not displayed in the timer.

--- src/modules/governance/components/Timer/Timer.tsx
+++ src/modules/governance/components/Timer/Timer.tsx
@@ -1,22 +1,16 @@
 import { Box, BoxProps } from '@material-ui/core';
-import React, { useState } from 'react';
-import { convertToDuration } from '../../../../common/utils/convertToDuration';
-import { useInterval } from '../../../../common/utils/useInterval';
-
-const ONE_SECOND = 1000;
+import React from 'react';
+import { t } from '../../../../common/utils/intl';
+import { useTimer } from './useTimer';
 
 interface ITimerProps extends BoxProps {
   endTime: Date;
 }
 
 export const Timer = ({ endTime, ...restProps }: ITimerProps) => {
-  const [duration, setDuration] = useState(
-    convertToDuration(new Date(), endTime),
+  const { duration, isTimeOver } = useTimer(endTime);
+
+  return (
+    <Box {...restProps}>{isTimeOver ? t('time.time-is-up') : duration}</Box>
   );
-
-  useInterval(() => {
-    setDuration(convertToDuration(new Date(), endTime));
-  }, ONE_SECOND);
-
-  return <Box {...restProps}>{duration}</Box>;
 };
