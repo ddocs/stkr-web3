de63a3eebc76421d28ad627660cb62074f2bcd88
Kud8 <Mitya-kudr@mail.ru>
2021-08-06 12:47:22 +0300

    STAKAN-108: add react-snap (#267)
    * STAKAN-108: add react-snap
    
    * STAKAN-108: update config
    
    * STAKAN-108: remove useless
    
    * STAKAN-108: add skipThirdPartyRequests
    
    * STAKAN-108: fix styles
    
    * STAKAN-108: asyncScriptTags + change font name
    
    * STAKAN-108: asyncScriptTags is true

--- src/components/AppBase/AppBase.tsx
+++ src/components/AppBase/AppBase.tsx
@@ -2,11 +2,14 @@ import React, { ReactNode, useEffect, useState } from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import intl from 'react-intl-universal';
 import { CssBaseline } from '@material-ui/core';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import { ReactReduxContext } from 'react-redux';
+import { create } from 'jss';
+import preset from 'jss-preset-default';
+
 import { locales } from '../../common/locales';
 import { mainTheme } from '../../common/themes/mainTheme';
 import { QueryLoadingAbsolute } from '../QueryLoading/QueryLoading';
-import { ReactReduxContext } from 'react-redux';
 import '../../common/fonts/stylesheet.css';
 import { historyInstance } from '../../common/utils/historyInstance';
 import { useLocale } from '../../common/utils/useLocale';
@@ -14,6 +17,16 @@ import { useLocale } from '../../common/utils/useLocale';
 interface IAppBaseProps {
   children: ReactNode;
 }
+
+const createGenerateClassName = () => {
+  let counter = 0;
+  return () =>
+    `c${
+      Math.random().toString(36).substring(2, 4) +
+      Math.random().toString(36).substring(2, 4)
+    }-${counter++}`;
+};
+const jss = create(preset());
 
 export const AppBase = ({ children }: IAppBaseProps) => {
   const [initDone, setInitDone] = useState(false);
@@ -33,16 +46,21 @@ export const AppBase = ({ children }: IAppBaseProps) => {
   }, [locale]);
 
   return (
-    <MuiThemeProvider theme={mainTheme}>
-      <CssBaseline />
+    <StylesProvider jss={jss} generateClassName={createGenerateClassName()}>
+      <MuiThemeProvider theme={mainTheme}>
+        <CssBaseline />
 
-      {!initDone ? (
-        <QueryLoadingAbsolute />
-      ) : (
-        <ConnectedRouter history={historyInstance} context={ReactReduxContext}>
-          {children}
-        </ConnectedRouter>
-      )}
-    </MuiThemeProvider>
+        {!initDone ? (
+          <QueryLoadingAbsolute />
+        ) : (
+          <ConnectedRouter
+            history={historyInstance}
+            context={ReactReduxContext}
+          >
+            {children}
+          </ConnectedRouter>
+        )}
+      </MuiThemeProvider>
+    </StylesProvider>
   );
 };
