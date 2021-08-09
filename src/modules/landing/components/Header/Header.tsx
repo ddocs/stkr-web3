ad9493fd177cfb9f0c2b55938aa21ec8b778a029
Kud8 <Mitya-kudr@mail.ru>
2021-08-09 10:56:20 +0300

    [STAKAN-111] Add Menu in Landing page (#270)

--- src/modules/landing/components/Header/Header.tsx
+++ src/modules/landing/components/Header/Header.tsx
@@ -9,7 +9,6 @@ interface HeaderProps {
   isMenuOpened: boolean;
 }
 
-// eslint-disable-next-line no-unused-vars
 const Header = ({ onMenuToggle, isMenuOpened }: HeaderProps) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
@@ -27,6 +26,9 @@ const Header = ({ onMenuToggle, isMenuOpened }: HeaderProps) => {
           </Link>
           <div className={classes.divider} />
           <div>Polkadot Parachain Bonds</div>
+        </div>
+        <div className={classes.menu} onClick={onMenuToggle}>
+          {isMenuOpened ? 'Close' : 'Menu'}
         </div>
       </div>
     </SectionWrapper>
